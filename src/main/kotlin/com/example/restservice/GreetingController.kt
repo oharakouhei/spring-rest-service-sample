e68cb19f7dd714dfafda8920370c5ebe3d5123b8
oharakouhei <gofue0723@gmail.com>
2021-02-20 12:08:36 +0900

    Add files for rest-service tutorial

---
+++ src/main/kotlin/com/example/restservice/GreetingController.kt
@@ -0,0 +1,20 @@
+package com.example.restservice
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+    private val counter = AtomicLong()
+
+    companion object {
+        private const val template = "Hello, %s!"
+    }
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String?): Greeting {
+        return Greeting(counter.incrementAndGet(), String.format(template, name))
+    }
+}
